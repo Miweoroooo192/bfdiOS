5d50d21ccc18e6746b562cb8d8937e575b8adf9a
.Miweoro <miweoro@outlook.pt>
2025-03-30 21:46:03 +0100

    added files by git

---
+++ Kernel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace bfdios2763test
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            if (input == "about")
+            {
+                Console.WriteLine("BFDI OS BETA VERSION 0.1 BUILD 15\nMade by Miweoro 2025");
+            }
+            else if (input == "exit")
+            {
+                Console.WriteLine("Exiting...");
+                Environment.Exit(0);
+            }
+            else if (input == "help")
+            {
+                Console.WriteLine("Commands:\nhelp: shows the help\nabout: shows the version of the OS\nexit: shutdown the OS");
+            }
+            else
+            {
+                Console.WriteLine(input + " is not a command");
+            }
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
